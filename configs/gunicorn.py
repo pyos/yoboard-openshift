695688b298f74eacdb65562b3361291880a93a46
pyos <pyos100500@gmail.com>
2014-05-28 19:33:07 +0400

    Load the app in each worker separately.

--- configs/gunicorn.py
+++ configs/gunicorn.py
@@ -7,7 +7,6 @@ bind         = 'unix:/tmp/gunicorn.socket'
 workers      = 4
 worker_class = Worker
 daemon       = True
-preload_app  = True
 
 accesslog = path.join(environ['OPENSHIFT_DIY_LOG_DIR'], 'gunicorn.access.log')
 errorlog  = path.join(environ['OPENSHIFT_DIY_LOG_DIR'], 'gunicorn.errors.log')
